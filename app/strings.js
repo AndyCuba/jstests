068543a5f39e7a31f2bbe65ab757b92e3c989fbc
Andy Daytona <zfesandydaytona@gmail.com>
2021-09-26 13:44:28 +0300

    [jstests] levels 12-13 finished

--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,36 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    return str.replace(/(\w)\1+/g, '$1'.repeat(amount));
   },
 
   wordWrap: function(str, cols) {
+    var formatString = '',
+      wordsArray = [];
 
+
+    wordsArray = str.split(' ');
+
+    formatString = wordsArray[0];
+
+    for (var i = 1; i < wordsArray.length; i++) {
+      if (wordsArray[i].length > cols) {
+        formatString += '\n' + wordsArray[i];
+      } else {
+        // eslint-disable-next-line no-lonely-if
+        if (formatString.length + wordsArray[i].length > cols) {
+          formatString += '\n' + wordsArray[i];
+        } else {
+          formatString += ' ' + wordsArray[i];
+        }
+      }
+    }
+
+    return formatString;
   },
 
   reverseString: function(str) {
-
+    return str.split('').reverse().
+      join('');
   }
 };
